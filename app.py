4174dc1bd1e33c36c1c0ee72778baac824550c15
somfist <qkrruddjs317@naver.com>
2022-07-13 18:15:58 +0900

    app.py 주석 해제

--- app.py
+++ app.py
@@ -23,64 +23,63 @@ db = client.dbsparta
 
 @app.route('/')
 def home():
-    return render_template('index.html')
-    # token_receive = request.cookies.get('mytoken')
-    # try:
-    #     payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-    #
-    #     return render_template('login.html')
-    # except jwt.ExpiredSignatureError:
-    #     return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
-    # except jwt.exceptions.DecodeError:
-    #     return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+
+        return render_template('login.html')
+    except jwt.ExpiredSignatureError:
+        return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
+    except jwt.exceptions.DecodeError:
+        return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
 
 
-# @app.route('/login')
-# def login():
-#     # msg = request.args.get("msg")
-#     # return render_template('login.html', msg=msg)
-#
-# @app.route('/sign_in', methods=['POST'])
-# def sign_in():
-#     # 로그인
-#     username_receive = request.form['username_give']
-#     password_receive = request.form['password_give']
-#
-#     pw_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
-#     result = db.users.find_one({'username': username_receive, 'password': pw_hash})
-#
-#     if result is not None:
-#         payload = {
-#             'id': username_receive,
-#             'exp': datetime.utcnow() + timedelta(seconds=60 * 60 * 24)  # 로그인 24시간 유지
-#         }
-#         token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
-#
-#         return jsonify({'result': 'success', 'token': token})
-#
-#     # 찾지 못하면
-#     else:
-#         return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
-#
-#
-# @app.route('/sign_up/save', methods=['POST'])
-# def sign_up():
-#     username_receive = request.form['username_give']
-#     password_receive = request.form['password_give']
-#     password_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
-#     doc = {
-#         "username": username_receive,  # 아이디
-#         "password": password_hash  # 비밀번호
-#     }
-#     db.users.insert_one(doc)
-#     return jsonify({'result': 'success'})
-#
-#
-# @app.route('/sign_up/check_dup', methods=['POST'])
-# def check_dup():
-#     username_receive = request.form['username_give']
-#     exists = bool(db.users.find_one({"username": username_receive}))
-#     return jsonify({'result': 'success', 'exists': exists})
+@app.route('/login')
+def login():
+    msg = request.args.get("msg")
+    return render_template('login.html', msg=msg)
+
+@app.route('/sign_in', methods=['POST'])
+def sign_in():
+    # 로그인
+    username_receive = request.form['username_give']
+    password_receive = request.form['password_give']
+
+    pw_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
+    result = db.users.find_one({'username': username_receive, 'password': pw_hash})
+
+    if result is not None:
+        payload = {
+            'id': username_receive,
+            'exp': datetime.utcnow() + timedelta(seconds=60 * 60 * 24)  # 로그인 24시간 유지
+        }
+        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
+
+        return jsonify({'result': 'success', 'token': token})
+
+    # 찾지 못하면
+    else:
+        return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
+
+
+@app.route('/sign_up/save', methods=['POST'])
+def sign_up():
+    username_receive = request.form['username_give']
+    password_receive = request.form['password_give']
+    password_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
+    doc = {
+        "username": username_receive,  # 아이디
+        "password": password_hash  # 비밀번호
+    }
+    db.users.insert_one(doc)
+    return jsonify({'result': 'success'})
+
+
+@app.route('/sign_up/check_dup', methods=['POST'])
+def check_dup():
+    username_receive = request.form['username_give']
+    exists = bool(db.users.find_one({"username": username_receive}))
+    return jsonify({'result': 'success', 'exists': exists})
 
 @app.route('/mypage')
 def mypage():
@@ -88,10 +87,10 @@ def mypage():
 
 @app.route('/update_profile', methods=['POST'])
 def save_img():
-    # token_receive = request.cookies.get('mytoken')
-    # try:
-    #     payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-    #     username = payload["id"]
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        username = payload["id"]
         about_receive = request.form["about_give"]
         new_doc = {
             "profile_info": about_receive
@@ -100,16 +99,14 @@ def save_img():
             file = request.files["file_give"]
             filename = secure_filename(file.filename)
             extension = filename.split(".")[-1]
-            # file_path = f"profile_pics/{username}.{extension}"
-            file_path = f"profile_pics/test.{extension}"
+            file_path = f"profile_pics/{username}.{extension}"
             file.save("./static/"+file_path)
             new_doc["profile_pic"] = filename
             new_doc["profile_pic_real"] = file_path
-        # db.users.update_one({'username': payload['id']}, {'$set':new_doc})
-        db.users.update_one({'username': 'bob'}, {'$set':new_doc})
+        db.users.update_one({'username': payload['id']}, {'$set':new_doc})
         return jsonify({"result": "success", 'msg': '프로필을 업데이트했습니다.'})
-    # except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
-    #     return redirect(url_for("home"))
+    except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
+        return redirect(url_for("home"))
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)

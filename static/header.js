2dd5d0cde9c2b144b9b245b4bec69186c776c632
JongOne <cowhddnjsdl0@naver.com>
2022-07-14 10:46:25 +0900

    재철님 부분 반영

--- static/header.js
+++ static/header.js
@@ -1,8 +1,10 @@
+msgCheck()
 $(document).ready(
     function animated() {
         for (i = 0; i < $('.icos').length; i++) {
             let target = $('.icos')[i]
-            let url = $(target).css('background-image').split('Melody')[1].split('.')[0]
+            let url = $(target).css('background-image').replace(/^url\(['"](.+)['"]\)/, '$1');
+            url = url.split(window.origin)[1].split('.')[0]
             target.addEventListener('mouseover', function move() {
                 let newUrl = '../' + url + '.gif'
                 $(target).css('background-image', 'url(' + newUrl + ')')
@@ -12,5 +14,71 @@ $(document).ready(
                 $(target).css('background-image', 'url(' + newUrl + ')')
             })
         }
-    }
+    },
+
 )
+
+
+
+function post() {
+    let url = $('#url').val()
+    let category = $('#category').val()
+    let comment = $('#comment').val()
+    $.ajax({
+        type: 'POST',
+        url: '/post',
+        data: {url_give: url, comment_give: comment, category_give: category},
+        success: function (response) {
+            alert(response['msg'])
+            window.location.reload()
+        }
+    });
+    console.log(url, comment)
+}
+
+$(document).ready(function () {
+            show_musics();
+        });
+
+        function show_musics() {
+            $('#cards-box').empty()
+            $.ajax({
+                type: 'GET',
+                url: '/musics',
+                data: {},
+                success: function (response) {
+                    console.log(response)
+                    let rows = response['musics']
+                    for (let i = 0; i < rows.length; i++) {
+                        let url = rows[i]['url']
+                        let comment = rows[i]['comment']
+                        let like = rows[i]['like']
+                        let aa = url.split('/',4)[3]
+                        let temp_html = `<div class="col">
+                                            <div class="card">
+                                                
+                                                <a href="banner.html"> <img src="https://i.ytimg.com/vi_webp/${aa}/maxresdefault.webp"
+                                                                            class="card-img-top" width="300px" alt="..."> </a>
+                            
+                                                <div class="card-body">
+                                                    <h5 class="card-title">${url}</h5>
+                                                    <p class="card-text">${comment}</p>
+                                                    <span class="material-symbols-outlined">favorite</span>
+                                                    <p class="like">${like}</p>
+                                                </div>
+                                            </div>
+                                        </div>`
+                        $('#cards-box').append(temp_html)
+                    }
+                }
+            });
+        }
+
+function msgCheck() {
+    const url = new URL(window.location.href)
+    const urlParams = url.searchParams
+    if(urlParams.get('msg') != null) {
+        alert(urlParams.get('msg'))
+    }
+}
+
